98b789c4de6b6c1561c883b280d2e5df20317528
danilminer234 <42529310+danilminer234@users.noreply.github.com>
2024-01-14 01:38:04 +0100

    changed photo orfinary

--- pages/index.js
+++ pages/index.js
@@ -5,14 +5,14 @@ import 'react-image-lightbox/style.css';
 export default function Component() {
   // States for Photo Gallery
   const [images, setImages] = useState([
-    '/IMG_8624.JPG', 
     '/IMG_8612.JPG', 
+    '/IMG_8624.JPG', 
     '/IMG_8604.JPG', 
     '/IMG_8595.JPG', 
     '/IMG_8588.JPG', 
-    '/IMG_8583.JPG', 
+    '/IMG_8583.JPG',
+    '/IMG_8545.JPG',
     '/IMG_8560.JPG', 
-    '/IMG_8545.JPG'
   ]); // Add your image pathss
   const [photoIndex, setPhotoIndex] = useState(0);
   const [isOpen, setIsOpen] = useState(false);
